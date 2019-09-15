cf3fcfbd7de8720067f906465bea9a6f209c1e41
DerWaldmeister <fa8593@aol.com>
2019-09-15 16:39:47 +0200

    cleaned comments

--- convnet_2d.py
+++ convnet_2d.py
@@ -12,23 +12,17 @@ def create2dConvNetNeuralNetworkModel(input_size, output_size, learningRate):
     # TODO: Check learning rate
 
     convnet = input_data(shape=[None, input_size, input_size,1], name='input')
-    # convnet = tflearn.embedding(convnet, input_dim=input_size, output_dim=3)
 
     tflearn.init_graph(num_cores=1, gpu_memory_fraction=0.5)
 
-    # convolutionalNetwork = conv_1d(convolutionalNetwork, 2, 1, activation='relu')
     convnet = conv_2d(convnet, 32, 2, activation='relu')
-    # convolutionalNetwork = max_pool_1d(convolutionalNetwork, 2)
     convnet = max_pool_2d(convnet, 2)
 
-    # convolutionalNetwork = conv_1d(convolutionalNetwork, 4, 1, activation='relu')
     convnet = conv_2d(convnet, 64, 2, activation='relu')
-    # convolutionalNetwork = max_pool_1d(convolutionalNetwork, 2)
     convnet = max_pool_2d(convnet, 2)
 
     #TODO: Flatten?
 
-    # convolutionalNetwork = fully_connected(convolutionalNetwork, 4 , activation='relu')
     convnet = fully_connected(convnet, 1024, activation='relu')
     convnet = dropout(convnet, 0.8)
 
