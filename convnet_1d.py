35965473717e3f8725027d6ac0f3599e0bd88078
DerWaldmeister <fa8593@aol.com>
2019-10-21 16:16:59 +0200

    Added futureResourceUtilisation from Xiaolei

--- convnet_1d.py
+++ convnet_1d.py
@@ -3,31 +3,9 @@ from tflearn.layers.conv import conv_1d, max_pool_1d
 from tflearn.layers.core import input_data, dropout, fully_connected, flatten
 from tflearn.layers.estimator import regression
 
-'''
-def create1dConvNetNeuralNetworkModel(input_size, output_size, learningRate):
 
-    convnet = input_data(shape=[None, input_size], name='input')
-    convnet = tflearn.embedding(convnet, input_dim=input_size, output_dim=2)
-
-    convnet = conv_1d(convnet, nb_filter=10, filter_size=10, strides=1, padding='same', activation='relu')
-    convnet = max_pool_1d(convnet, kernel_size=3, strides=2, padding='valid')
-
-    convnet = conv_1d(convnet, 6, 2, activation='relu')
-    convnet = max_pool_1d(convnet, 3)
-
-    convnet = fully_connected(convnet, 9, activation='relu')
-    convnet = dropout(convnet, 0.8)
-
-    convnet = fully_connected(convnet, output_size, activation='softmax')
-    convnet = regression(convnet, optimizer='adam', learning_rate=learningRate, loss='categorical_crossentropy',
-                         name='targets')
-
-    model = tflearn.DNN(convnet, tensorboard_dir="log")
-
-    return model
-'''
-'''
 #CONFIGURATION 1
+'''
 def create1dConvNetNeuralNetworkModel(input_size, output_size, learningRate):
 
     tflearn.init_graph(num_cores=1, gpu_memory_fraction=0.7)
@@ -58,8 +36,9 @@ def create1dConvNetNeuralNetworkModel(input_size, output_size, learningRate):
     return model
 
 '''
-'''
+
 #CONFIGURATION 2
+'''
 def create1dConvNetNeuralNetworkModel(input_size, output_size, learningRate):
 
     convnet = input_data(shape=[None, input_size], name='input')
@@ -91,9 +70,9 @@ def create1dConvNetNeuralNetworkModel(input_size, output_size, learningRate):
     return model
 '''
 
-'''
-#CONFIGURATION 3
 
+#CONFIGURATION 3
+'''
 def create1dConvNetNeuralNetworkModel(input_size, output_size, learningRate):
     tflearn.init_graph(num_cores=1, gpu_memory_fraction=0.7)
 
@@ -123,7 +102,9 @@ def create1dConvNetNeuralNetworkModel(input_size, output_size, learningRate):
     return model
 
 '''
+
 #CONFIGURATION 4
+'''
 def create1dConvNetNeuralNetworkModel(input_size, output_size, learningRate):
 
     convnet = input_data(shape=[None, input_size], name='input')
@@ -153,3 +134,110 @@ def create1dConvNetNeuralNetworkModel(input_size, output_size, learningRate):
     model = tflearn.DNN(convnet, tensorboard_dir='log')
 
     return model
+
+'''
+
+#CONFIGURATION 5
+'''
+def create1dConvNetNeuralNetworkModel(input_size, output_size, learningRate):
+    tflearn.init_graph(num_cores=1, gpu_memory_fraction=0.7)
+
+    convnet = input_data(shape=[None, input_size], name='input')
+    convnet = tflearn.embedding(convnet, input_dim=input_size, output_dim=2)
+
+    convnet = conv_1d(convnet, nb_filter=10, filter_size=5, strides=1, padding='same', activation='relu')
+    convnet = max_pool_1d(convnet, kernel_size=2, strides=1, padding='valid')
+
+    convnet = conv_1d(convnet, nb_filter=20, filter_size=3, strides=1, padding='valid', activation='relu')
+    convnet = max_pool_1d(convnet, kernel_size=2, strides=1, padding='valid')
+
+    convnet = flatten(convnet)
+
+    convnet = fully_connected(convnet, n_units=1800, weights_init='truncated_normal', activation='relu')
+    convnet = dropout(convnet, 0.8)
+
+    convnet = fully_connected(convnet, n_units=900, weights_init='truncated_normal', activation='relu')
+    convnet = dropout(convnet, 0.8)
+
+    convnet = fully_connected(convnet, n_units=output_size, activation='softmax')
+    convnet = regression(convnet, optimizer='adam', learning_rate=learningRate, loss='categorical_crossentropy',
+                         name='targets')
+
+    model = tflearn.DNN(convnet, tensorboard_dir='log')
+
+    return model
+
+'''
+#CONFIGURATION 6
+'''
+def create1dConvNetNeuralNetworkModel(input_size, output_size, learningRate):
+
+    convnet = input_data(shape=[None, input_size], name='input')
+    convnet = tflearn.embedding(convnet, input_dim=input_size, output_dim=2)
+
+    convnet = conv_1d(convnet, nb_filter=10, filter_size=7, strides=1, padding='same', activation='relu')
+    convnet = max_pool_1d(convnet, kernel_size=2, strides=1, padding='valid')
+
+    convnet = conv_1d(convnet, nb_filter=20, filter_size=5, strides=1, padding='valid', activation='relu')
+    convnet = max_pool_1d(convnet, kernel_size=2, strides=1, padding='valid')
+
+    convnet = conv_1d(convnet, nb_filter=30, filter_size=3, strides=1, padding='valid', activation='relu')
+    convnet = max_pool_1d(convnet, kernel_size=2, strides=1, padding='valid')
+
+    convnet = flatten(convnet)
+
+    convnet = fully_connected(convnet, n_units=1800, weights_init='truncated_normal', activation='relu')
+    convnet = dropout(convnet, 0.8)
+
+    convnet = fully_connected(convnet, n_units=900, weights_init='truncated_normal', activation='relu')
+    convnet = dropout(convnet, 0.8)
+
+    convnet = fully_connected(convnet, n_units=output_size, activation='softmax')
+    convnet = regression(convnet, optimizer='adam', learning_rate=learningRate, loss='categorical_crossentropy',
+                         name='targets')
+
+    model = tflearn.DNN(convnet, tensorboard_dir='log')
+
+    return model
+
+'''
+
+
+#CONFIGURATION 7
+
+def create1dConvNetNeuralNetworkModel(input_size, output_size, learningRate):
+
+    convnet = input_data(shape=[None, input_size], name='input')
+    convnet = tflearn.embedding(convnet, input_dim=input_size, output_dim=2)
+
+    convnet = conv_1d(convnet, nb_filter=10, filter_size=3, strides=1, padding='same', activation='relu')
+    convnet = max_pool_1d(convnet, kernel_size=2, strides=1, padding='valid')
+
+    convnet = conv_1d(convnet, nb_filter=15, filter_size=3, strides=1, padding='valid', activation='relu')
+    convnet = max_pool_1d(convnet, kernel_size=2, strides=1, padding='valid')
+
+    convnet = conv_1d(convnet, nb_filter=20, filter_size=2, strides=1, padding='valid', activation='relu')
+    convnet = max_pool_1d(convnet, kernel_size=2, strides=1, padding='valid')
+
+    convnet = conv_1d(convnet, nb_filter=25, filter_size=2, strides=1, padding='valid', activation='relu')
+    convnet = max_pool_1d(convnet, kernel_size=2, strides=1, padding='valid')
+
+    convnet = flatten(convnet)
+
+    convnet = fully_connected(convnet, n_units=1800, weights_init='truncated_normal', activation='relu')
+    convnet = dropout(convnet, 0.8)
+
+    convnet = fully_connected(convnet, n_units=900, weights_init='truncated_normal', activation='relu')
+    convnet = dropout(convnet, 0.8)
+
+    convnet = fully_connected(convnet, n_units=450, weights_init='truncated_normal', activation='relu')
+    convnet = dropout(convnet, 0.8)
+
+    convnet = fully_connected(convnet, n_units=output_size, activation='softmax')
+    convnet = regression(convnet, optimizer='adam', learning_rate=learningRate, loss='categorical_crossentropy',
+                         name='targets')
+
+    model = tflearn.DNN(convnet, tensorboard_dir='log')
+
+    return model
+
--- env.py
+++ env.py
@@ -298,12 +298,11 @@ def runSimulation(runSimulation_input):
                     activityConversionVector = indexActivitiesGlobal_reordered
 
             # 1.4 normalized state vector and matrix are created
-            # distinguish between 1dimensional convnet and 2dimensional convnet (vector vs matrix)
-            #if neuralNetworkType != "2dimensional convnet" or neuralNetworkType == None:
-            if neuralNetworkType == "1dimensional convnet" or neuralNetworkType == None:
-                currentState_readyToStartActivities = []
-
-                if trivialDecision == False:
+            # NEW: trivial decision check before neural network type check
+            if trivialDecision == False:
+                # distinguish between 1dimensional convnet and 2dimensional convnet (vector vs matrix)
+                if neuralNetworkType == "1dimensional convnet" or neuralNetworkType == None:
+                    currentState_readyToStartActivities = []
 
                     currentState_readyToStartActivities = np.zeros([stateVectorLength])
 
@@ -323,43 +322,155 @@ def runSimulation(runSimulation_input):
                         currentActivitySequence.availableResources[resourceConversionVector[j]] / \
                         currentActivitySequence.totalResources[resourceConversionVector[j]]
 
-                            # Xiaolei:
-                            # 1.4.1 add future resourceUtilisation for active activities
+                # neuralNetworkType = "2dimensional convnet"
+                elif neuralNetworkType == "2dimensional convnet":
+                    currentState_readyToStartActivitiesMatrix = [[]]
+                    if trivialDecision == False:
+                        currentState_readyToStartActivities = np.zeros((1, stateVectorLength))
+                        for i, indexActivity in enumerate(activityConversionVector):
+                            if indexActivity != -1:
+                                currentState_readyToStartActivities[0, 0 + i * (1 + numberOfResources)] = \
+                                    currentActivitySequence.activities[indexActivity].time * rescaleFactorTime
+                                for j in range(numberOfResources):
+                                    currentState_readyToStartActivities[0, 1 + j + i * (1 + numberOfResources)] = \
+                                        currentActivitySequence.activities[indexActivity].requiredResources[
+                                            resourceConversionVector[j]] / currentActivitySequence.totalResources[
+                                            resourceConversionVector[j]]
+                        for j in range(numberOfResources):
+                            currentState_readyToStartActivities[
+                                0, numberOfActivitiesInStateVector + numberOfActivitiesInStateVector * numberOfResources + j] = \
+                                currentActivitySequence.availableResources[resourceConversionVector[j]] / \
+                                currentActivitySequence.totalResources[resourceConversionVector[j]]
 
-            # neuralNetworkType = "2dimensional convnet"
-            #elif neuralNetworkType != "1dimensional convnet" and neuralNetworkType != None:
-            elif neuralNetworkType == "2dimensional convnet":
-                currentState_readyToStartActivitiesMatrix = [[]]
-                if trivialDecision == False:
-                    currentState_readyToStartActivities = np.zeros((1, stateVectorLength))
-                    for i, indexActivity in enumerate(activityConversionVector):
-                        if indexActivity != -1:
-                            currentState_readyToStartActivities[0, 0 + i * (1 + numberOfResources)] = \
-                                currentActivitySequence.activities[indexActivity].time * rescaleFactorTime
-                            for j in range(numberOfResources):
-                                currentState_readyToStartActivities[0, 1 + j + i * (1 + numberOfResources)] = \
-                                    currentActivitySequence.activities[indexActivity].requiredResources[
-                                        resourceConversionVector[j]] / currentActivitySequence.totalResources[
-                                        resourceConversionVector[j]]
-                    for j in range(numberOfResources):
-                        currentState_readyToStartActivities[
-                            0, numberOfActivitiesInStateVector + numberOfActivitiesInStateVector * numberOfResources + j] = \
-                            currentActivitySequence.availableResources[resourceConversionVector[j]] / \
-                            currentActivitySequence.totalResources[resourceConversionVector[j]]
+                        # multiply the currentState_readyToStartActivities vector with its transpose vector in order to get a matrix
+                        # currentState_readyToStartActivities is in theformat of (1, stateVectorLength)
+                        currentState_readyToStartActivitiesVertical = currentState_readyToStartActivities.reshape(
+                            stateVectorLength, 1)
+                        currentState_readyToStartActivitiesMatrix = np.matmul(
+                            currentState_readyToStartActivitiesVertical, currentState_readyToStartActivities)
 
-                    # Multiply the currentState_readyToStartActivities vector with its transpose vector in order to get a matrix
-                    # currentState_readyToStartActivities is in theformat of (1, stateVectorLength)
-                    currentState_readyToStartActivitiesVertical = currentState_readyToStartActivities.reshape(
-                        stateVectorLength, 1)
-                    currentState_readyToStartActivitiesMatrix = np.matmul(
-                        currentState_readyToStartActivitiesVertical, currentState_readyToStartActivities)
-
-                #else:
-                    #currentState_readyToStartActivities = []
-                    #currentState_readyToStartActivitiesMatrix = [[]]
-            else:
-                print("Fehler")
-
+                    #else:
+                        #currentState_readyToStartActivities = []
+                        #currentState_readyToStartActivitiesMatrix = [[]]
+                else:
+                    print("Fehler")
+                ''' 
+                # 1.4.1 add future resourceUtilisation for active activities
+                # the following code was provied by https://github.com/leiiiiii/RCPSP/blob/master/Env.py
+                indexReadyToActiveActivities = []
+                # indexAlreadyStartedActivities=[]
+                # for i in range(numberOfActivities):
+                #     if currentActivitySequence.activities[i].withToken and currentActivitySequence.activities[i].idleToken == False:
+                #         indexAlreadyStartedActivities.append(i)
+    
+                # add already started activities in indexStartToActiveActivities
+                # for j in indexAlreadyStartedActivities:
+                #     indexStartToActiveActivities.append(j)
+    
+                # add ready to start activities in indexStartToActiveActivities
+                for n in indexReadyToStartActivitiesInState:
+                    indexReadyToActiveActivities.append(n)
+    
+                # generate timeHorizonMatrix for ReadytoActive activities
+                timeHorizonMatrix = np.zeros((len(indexReadyToActiveActivities), timeHorizon))
+                timeUnitmatrix = [x for x in range(len(indexReadyToActiveActivities))]
+                remainingtimeList = []
+                for i in indexReadyToActiveActivities:
+                    remainingtimeList.append(currentActivitySequence.activities[i].remainingTime)
+                for value in remainingtimeList:
+                    if value > timeHorizon:
+                        value = timeHorizon
+                maximaltimeHorizon = max(remainingtimeList)
+                for (i, j) in zip(timeUnitmatrix, remainingtimeList):
+                    timeHorizonMatrix[i][0:j] = 1
+    
+                # generate resourceUtilizationMatrix for ReadytoActive activities
+                resourcematrix = np.zeros((1, numberOfResources))
+    
+                for i in indexReadyToActiveActivities:
+                    a = [currentActivitySequence.activities[i].requiredResources]
+                    resourcematrix = np.concatenate((resourcematrix, a), axis=0)
+                resourcematrix = resourcematrix[1:]
+                resourceUtilizationMatrix = resourcematrix.T
+    
+                # currentState_futureResourceUtilisation for ReadytoActive activity generated
+                currentState_futureResourceUtilisation_forActive = np.dot(resourceUtilizationMatrix,
+                                                                          timeHorizonMatrix)
+    
+                # 1.4.2 add future resourceUtilisation for following activities
+                for i in indexReadyToActiveActivities:
+                    indexFollowingActivities = currentActivitySequence.activities[i].indexFollowingActivities
+    
+                # generate timeHorizonMatrix for following activities (timeHorizon starts maximaltimeHorizon)
+                if len(indexFollowingActivities) == 0:
+                    currentState_futureResourceUtilisation_forFollowing = np.zeros(
+                        (numberOfResources, timeHorizon))
+    
+                else:
+                    timeHorizonMatrixforFollowing = np.zeros((len(indexFollowingActivities), timeHorizon))
+    
+                    if len(indexFollowingActivities) > 1:
+                        timeUnitmatrixforFollowing = [x for x in range(len(indexFollowingActivities))]
+                        timeListforFollowing = []
+                        for i in indexFollowingActivities:
+                            timeListforFollowing.append(currentActivitySequence.activities[i].time)
+    
+                        for value in timeListforFollowing:
+                            if value + maximaltimeHorizon > timeHorizon:
+                                value = timeHorizon - maximaltimeHorizon
+    
+                        for (i, j) in zip(timeUnitmatrixforFollowing, timeListforFollowing):
+                            timeHorizonMatrixforFollowing[i][maximaltimeHorizon:j] = 1
+    
+                    else:
+                        for i in indexFollowingActivities:
+                            index = currentActivitySequence.activities[i].time
+                        if index + maximaltimeHorizon > 10:
+                            index = timeHorizon - maximaltimeHorizon
+                            timeHorizonMatrixforFollowing[0][maximaltimeHorizon:index] = 1
+    
+                    # generate resourceUtilizationMatrix for following activities
+                    resourcematrixforFollowing = np.zeros((1, numberOfResources))
+                    if len(indexFollowingActivities) > 1:
+                        for i in indexFollowingActivities:
+                            a = [currentActivitySequence.activities[i].requiredResources]
+                            resourcematrixforFollowing = np.concatenate((resourcematrixforFollowing, a), axis=0)
+                        resourcematrixforFollowing = resourcematrixforFollowing[1:]
+                        resourceUtilizationMatrixforFollowing = resourcematrixforFollowing.T
+    
+                    elif len(indexFollowingActivities) == 1:
+                        for i in indexFollowingActivities:
+                            currentActivitySequence.activities[i].requiredResources = np.array(
+                                currentActivitySequence.activities[i].requiredResources)
+                            resourceUtilizationMatrixforFollowing = currentActivitySequence.activities[
+                                i].requiredResources.reshape((numberOfResources, 1))
+    
+                    # currentState_futureResourceUtilisation for following activities generated
+                    currentState_futureResourceUtilisation_forFollowing = np.dot(
+                        resourceUtilizationMatrixforFollowing, timeHorizonMatrixforFollowing)
+    
+                currentState_futureResourceUtilisation = np.add(
+                    currentState_futureResourceUtilisation_forActive,
+                    currentState_futureResourceUtilisation_forFollowing)
+    
+                for i in range(numberOfResources):
+                    for j in range(timeHorizon):
+                        currentState_futureResourceUtilisation[i][j] = \
+                            currentState_futureResourceUtilisation[i][j] / currentActivitySequence.totalResources[i]
+    
+                resourceConversionVector = np.array(resourceConversionVector)
+    
+                currentState_futureResourceUtilisation = currentState_futureResourceUtilisation[
+                    resourceConversionVector]
+    
+                currentStateFuturnResourceUtilisation = currentState_futureResourceUtilisation.flatten()
+                
+                
+                for i in range(timeHorizon * numberOfResources):
+                    currentState_readyToStartActivities[
+                        numberOfActivitiesInStateVector + numberOfActivitiesInStateVector * numberOfResources + numberOfResources + i] = \
+                        currentStateFuturnResourceUtilisation[i]
+                '''
 
             # 1.5 Use the policy and the decision tool to define which tokens can begin the correspondent activity or remain idle
             randomDecisionAtThisStep = (random.random() < randomDecisionProbability)
