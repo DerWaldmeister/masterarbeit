fb7d7a3fa659b0d90a6fff441a72e248cfead89d
DerWaldmeister <fa8593@aol.com>
2019-12-08 16:45:11 +0100

    Created more logs

--- convnet_2d.py
+++ convnet_2d.py
@@ -117,7 +117,7 @@ def create2dConvNetNeuralNetworkModel(input_size, output_size, learningRate):
 '''
 
 #CONFIGURATION 4a
-
+'''
 def create2dConvNetNeuralNetworkModel(input_size, output_size, learningRate):
 
     # Specify the log directory
@@ -128,6 +128,37 @@ def create2dConvNetNeuralNetworkModel(input_size, output_size, learningRate):
     #tflearn.init_graph(num_cores=1, gpu_memory_fraction=0.9)
 
     convnet = conv_2d(convnet, nb_filter=16, filter_size=3, strides=1, padding='valid', activation='relu')
+    convnet = max_pool_2d(convnet, kernel_size=2, strides=2, padding='valid')
+
+    convnet = conv_2d(convnet, nb_filter=32, filter_size=3, strides=1, padding='valid', activation='relu')
+    convnet = max_pool_2d(convnet, kernel_size=2, strides=2, padding='valid')
+
+    convnet = flatten(convnet)
+
+    convnet = fully_connected(convnet, n_units=256, weights_init='truncated_normal', activation='relu')
+    convnet = dropout(convnet, 0.5)
+
+    convnet = fully_connected(convnet, n_units=output_size, activation='softmax')
+    convnet = regression(convnet, optimizer='adam', learning_rate=learningRate, loss='categorical_crossentropy', name='targets')
+
+    model = tflearn.DNN(convnet, tensorboard_dir=logdir)
+
+    return model
+    
+'''
+
+#CONFIGURATION 5
+
+def create2dConvNetNeuralNetworkModel(input_size, output_size, learningRate):
+
+    # Specify the log directory
+    logdir = 'log/2d/' + datetime.now().strftime('%Y%m%d-%H%M%S')
+
+    convnet = input_data(shape=[None, input_size, input_size,1], name='input_currentState')
+
+    #tflearn.init_graph(num_cores=1, gpu_memory_fraction=0.9)
+
+    convnet = conv_2d(convnet, nb_filter=16, filter_size=5, strides=1, padding='valid', activation='relu')
     convnet = max_pool_2d(convnet, kernel_size=2, strides=2, padding='valid')
 
     convnet = conv_2d(convnet, nb_filter=32, filter_size=3, strides=1, padding='valid', activation='relu')
