64c29b39862e1791d458be13b8d2ee95c42e1edb
DerWaldmeister <fa8593@aol.com>
2019-12-27 00:30:03 +0100

    Added combinend 1d configuration

--- combined_convnet_1d.py
+++ combined_convnet_1d.py
@@ -7,8 +7,8 @@ from tflearn.layers.merge_ops import merge, merge_outputs
 from datetime import datetime
 
 
-#CONFIGURATION1
-'''
+#CONFIGURATION2a
+
 def createCombined1dConvNetNeuralNetworkModelForFutureResourceUtilisation(input_size_states, output_size_actions,
                                                                               learningRate, rowsFutureResourceUtilisationMatrix, columnsFutureResourceUtilisationMatrix
                                                                               ):
@@ -27,12 +27,11 @@ def createCombined1dConvNetNeuralNetworkModelForFutureResourceUtilisation(input_
 
     convnetCurrentState = flatten(convnetCurrentState)
 
-
     #### 2d-convolutional layers for FutureResourceUtilisationMatrix ####
     # How to configure input_data: https://stackoverflow.com/questions/48482746/tflearn-what-is-input-data
     convnetResourceUtilisation = input_data(shape=[None, rowsFutureResourceUtilisationMatrix, columnsFutureResourceUtilisationMatrix, 1], name='input_futureResourceUtilisationMatrix')
 
-    convnetResourceUtilisation = conv_2d(convnetResourceUtilisation, nb_filter=16, filter_size=[4, 1], strides=1, padding='same', activation='relu')
+    convnetResourceUtilisation = conv_2d(convnetResourceUtilisation, nb_filter=16, filter_size=[4, 3], strides=1, padding='same', activation='relu')
     convnetResourceUtilisation = max_pool_2d(convnetResourceUtilisation, kernel_size=[4, 2], strides=2, padding='valid')
 
     convnetResourceUtilisation = flatten(convnetResourceUtilisation)
@@ -42,51 +41,6 @@ def createCombined1dConvNetNeuralNetworkModelForFutureResourceUtilisation(input_
 
     #### final fully connected layers ####
     finalNet = fully_connected(finalNet, n_units=128, weights_init='truncated_normal', activation='relu')
-    finalNet = dropout(finalNet, 0.5)
-
-    finalNet = fully_connected(finalNet, n_units=output_size_actions, activation='softmax')
-    finalNet = regression(finalNet, optimizer='adam', learning_rate=learningRate, loss='categorical_crossentropy', name='targets')
-
-    model = tflearn.DNN(finalNet, tensorboard_dir=logdir, tensorboard_verbose=0)
-
-    return model
-'''
-
-#CONFIGURATION2
-
-def createCombined1dConvNetNeuralNetworkModelForFutureResourceUtilisation(input_size_states, output_size_actions,
-                                                                              learningRate, rowsFutureResourceUtilisationMatrix, columnsFutureResourceUtilisationMatrix
-                                                                              ):
-
-    # Specify the log directory
-    logdir='log/1d_combined/' + datetime.now().strftime("%Y%m%d-%H%M%S")
-
-    tflearn.init_graph(num_cores=1, gpu_memory_fraction=0.8)
-
-    #### 1d-convolutional layers for currentState ####
-    convnetCurrentState = input_data(shape=[None, input_size_states], name='input_currentState')
-    convnetCurrentState = tflearn.embedding(convnetCurrentState, input_dim=input_size_states, output_dim=2)
-
-    convnetCurrentState = conv_1d(convnetCurrentState, nb_filter=16, filter_size=3, strides=1, padding='valid', activation='relu')
-    convnetCurrentState = max_pool_1d(convnetCurrentState, kernel_size=2, strides=2, padding='valid')
-
-    convnetCurrentState = flatten(convnetCurrentState)
-
-
-    #### 2d-convolutional layers for FutureResourceUtilisationMatrix ####
-    # How to configure input_data: https://stackoverflow.com/questions/48482746/tflearn-what-is-input-data
-    convnetResourceUtilisation = input_data(shape=[None, rowsFutureResourceUtilisationMatrix, columnsFutureResourceUtilisationMatrix, 1], name='input_futureResourceUtilisationMatrix')
-
-    convnetResourceUtilisation = conv_2d(convnetResourceUtilisation, nb_filter=16, filter_size=[4, 1], strides=1, padding='same', activation='relu')
-    convnetResourceUtilisation = max_pool_2d(convnetResourceUtilisation, kernel_size=[4, 2], strides=2, padding='valid')
-
-    convnetResourceUtilisation = flatten(convnetResourceUtilisation)
-
-    # merging the outputs of both convolutional nets
-    finalNet = merge_outputs([convnetResourceUtilisation, convnetCurrentState], 'concat') # axis=0 is concatenation
-
-    #### final fully connected layers ####
-    finalNet = fully_connected(finalNet, n_units=256, weights_init='truncated_normal', activation='relu')
     finalNet = dropout(finalNet, 0.5)
 
     finalNet = fully_connected(finalNet, n_units=output_size_actions, activation='softmax')
